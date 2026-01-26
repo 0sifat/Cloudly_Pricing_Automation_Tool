14682188d8cff1dbe0aae5021a2febe17f3c8b7f
Sadiqul Islam <104644991+Sadiq-code-nest@users.noreply.github.com>
2026-01-26 11:45:39 +0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
 require_once __DIR__ . '/init.php';
 
-$host = 'localhost';              // Docker service name
+$host = 'db';              // Docker service name
 $db   = 'aws_calc';
-$user = 'root';        // from docker-compose
-$pass = '';    // from docker-compose
-$port = 3307;              // internal MySQL port
+$user = 'app_user';        // from docker-compose
+$pass = 'app_password';    // from docker-compose
+$port = 3306;              // internal MySQL port
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
